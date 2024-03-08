53e353d7173667bfe793cab4503748061ac852f7
lucascampos04 <camposdlucasoli@gmail.com>
2024-03-08 14:59:47 -0300

    bubleSort

---
+++ python/array.py
@@ -0,0 +1,14 @@
+def bublleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+bublleSort(array)
+print(array)
+
+
